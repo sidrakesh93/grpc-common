0b7216490cd27b248f9e89d3c85f769dbcdaed6f
Siddharth Rakesh <sidrakesh@google.com>
2015-06-10 17:41:17 -0700

    re-arranged proto files

--- tools/performance_db/performance_db_frontend/app/run_codegen.sh
+++ tools/performance_db/performance_db_frontend/app/run_codegen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Runs the protoc with gRPC plugin to generate protocol messages and gRPC stubs.
-protoc -I . --python_out=. ./qpstest.proto
-protoc -I . --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` ./user_data.proto
+protoc -I ../../protos --python_out=. ../../protos/qpstest.proto
+protoc -I ../../protos --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../../protos/user_data.proto
